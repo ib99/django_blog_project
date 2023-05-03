4463fa0c87689e2f557d218c1a5c5f76a6634b96
ib99 <ib99@users.noreply.github.com>
2023-05-02 21:37:11 -0400

    nc

--- djblogger/djblogger/settings/local.py
+++ djblogger/djblogger/settings/local.py
@@ -1,3 +1,1 @@
-import math
-
 from .base import *
--- djblogger/manage.py
+++ djblogger/manage.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+
 from djblogger.settings import base
+
 
 def main():
     """Run administrative tasks."""
