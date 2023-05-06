42c3a3c071c69765cd401abecbb8f36c29f99700
ib99 <ib99@users.noreply.github.com>
2023-05-06 18:35:09 -0400

    new commit

--- djblogger/djblogger/blog/models.py
+++ djblogger/djblogger/blog/models.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 from taggit.managers import TaggableManager
 
 # Create your models here.
 
 
 class Post(models.Model):
-
     options = (
         ("draft", "Draft"),
         ("published", "Published"),
@@ -17,13 +17,16 @@ class Post(models.Model):
     slug = models.SlugField(max_length=250, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="post_author"
-        )
+    )
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=options, default="draft")
 
     tags = TaggableManager()
+
+    def get_absolute_url(self):
+        return reverse("post_single", args=[self.slug])
 
     class Meta:
         ordering = ("-created_at",)
